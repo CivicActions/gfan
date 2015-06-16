8d21d5b7a7ccd4b1fa9943fb5eee99b8599372da
Owen Barton <drupal@owenbarton.com>
2015-06-16 13:44:21 -0700

    Fix typo in call.php.

--- html/call.php
+++ html/call.php
@@ -7,7 +7,5 @@ $client = new Services_Twilio($account_sid, $auth_token);
 $call = $client->account->calls->create(
   '+18056856571',
   '+18056996099',
-
-  // Read TwiML at this URL when a call connects (hold music)
-  'http://gfan.civicactions.net/twiml.php',
+  'http://gfan.civicactions.net/twiml.php'
 );
